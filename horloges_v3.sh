c556345f5689c65f7967385b2ec49c550bac9c76
echkourine25 <61584703+echkourine25@users.noreply.github.com>
2023-02-15 21:58:02 +0100

    Update horloges_v3.sh

--- horloges_v3.sh
+++ horloges_v3.sh
@@ -5,6 +5,8 @@ then rm $html_file;
 fi;
 echo "<meta charset=\"UTF-8\"/>">>$html_file
 for i in "$@";
-do echo "<div style=\"display:inline-block;text-align:center;border:0px;\"><div><iframe src=\"http://vps818192.ovh.>
+do echo "<div style=\"display:inline-block;text-align:center;border:0px;\">\
+<div><iframe src=\"http://vps818192.ovh.net:8000/$i\" \
+height=\"245\" width=\"208\" title=\"$i\"</iframe></div></div>">>$html_file;
 done;
 xdg-open $html_file;
